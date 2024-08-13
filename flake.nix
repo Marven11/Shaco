050846e7104c4e14212c405713e7db4ea4a67bd6
Marven11 <110723864+Marven11@users.noreply.github.com>
2024-08-13 23:42:56 +0800

    Update nix shell and fix typo

--- flake.nix
+++ flake.nix
@@ -12,14 +12,27 @@
     utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      pythonPackages = pkgs.python311Packages;
     in
     {
       devShells.default = pkgs.mkShell {
+        name = "python-venv";
+        venvDir = "./.venv-nixos";
         buildInputs = with pkgs; [
+          pythonPackages.python
+          pythonPackages.venvShellHook
           cmake
           clang
           glibc.static
         ];
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -r requirements.txt
+        '';
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+        '';
       };
 
 
